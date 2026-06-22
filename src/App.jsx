bb791f1de522df3f434b0198abb9f2fcce7ca50c
ramamarye99-web <ramamarye99@gmail.com>
2026-06-22 11:22:36 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { createClient } from '@supabase/supabase-js';
 import io from 'socket.io-client';
 import HomeScreen from './components/HomeScreen';
 import DebateRoom from './components/DebateRoom';
